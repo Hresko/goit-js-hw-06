061218f1d8e0205ffa96aa97c956d73572368aa9
Hreskoo <hresko21@gmail.com>
2022-08-24 00:21:54 +0300

    Task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const loginFormRef = document.querySelector('form.login-form');
 
+const onLoginFormSubmit = event => {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+
+  if (!email.value) {
+    alert('Please enter your Email address!');
+    return;
+  }
+  if (!password.value) {
+    alert('Please enter your Password!');
+    return;
+  }
+
+  const userCredentialsObject = createUserCredentialsObject(email, password);
+  console.log(userCredentialsObject);
+  event.currentTarget.reset();
+};
+
+const createUserCredentialsObject = (email, password) => ({
+  [email.name]: email.value,
+  [password.name]: password.value,
+});
+
+loginFormRef.addEventListener('submit', onLoginFormSubmit);
